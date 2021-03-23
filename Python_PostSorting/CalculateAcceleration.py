e94297c52e66b80a4e34b057ab560fa90228315c
sarahtennant <stennan2@exseed.ed.ac.uk>
2021-03-23 15:05:10 +0000

    update to remove outliers

--- Python_PostSorting/CalculateAcceleration.py
+++ Python_PostSorting/CalculateAcceleration.py
@@ -77,7 +77,7 @@ def generate_acceleration_rewarded_trials(spike_data, recording_folder):
     for cluster in range(len(spike_data)):
         session_id = spike_data.at[cluster, "session_id"]
         cluster_index = spike_data.cluster_id.values[cluster] - 1
-        rates =  np.array(spike_data.iloc[cluster].spikes_in_time_reward[0].real)*10
+        rates =  np.array(spike_data.iloc[cluster].spikes_in_time_reward[0].real)
         speed = np.array(spike_data.iloc[cluster].spikes_in_time_reward[1].real)
         trials =  np.array(spike_data.iloc[cluster].spikes_in_time_reward[3].real)
         types =  np.array(spike_data.iloc[cluster].spikes_in_time_reward[4].real)
@@ -91,6 +91,8 @@ def generate_acceleration_rewarded_trials(spike_data, recording_folder):
         except (ValueError, TypeError):
                 continue
 
+
+        """
         # remove outliers
         rates_o =  pd.Series(rates)
         speed_o =  pd.Series(speed)
@@ -103,13 +105,32 @@ def generate_acceleration_rewarded_trials(spike_data, recording_folder):
         position = position_o[speed_o.between(speed_o.quantile(.05), speed_o.quantile(.95))] # without outliers
         trials = trials_o[speed_o.between(speed_o.quantile(.05), speed_o.quantile(.95))] # without outliers
         types = types_o[speed_o.between(speed_o.quantile(.05), speed_o.quantile(.95))] # without outliers
+        """
+
+
 
         if speed.size > 1:
             acceleration = np.diff(np.array(speed))
             acceleration = np.hstack((0, acceleration))
+
+            # remove acceleration outliers
+            mean_speed = np.nanmean(acceleration)
+            sd_speed = np.nanstd(acceleration)
+            upper_speed_sd = mean_speed+(sd_speed*3)
+
+            data = np.vstack((rates, speed, acceleration, position, trials, types))
+            data=data.transpose()
+            data = data[data[:,2] < upper_speed_sd,:]
+            rates = data[:,0]
+            speed = data[:,1]
+            acceleration = data[:,2]
+            position = data[:,3]
+            trials = data[:,4]
+            types = data[:,5]
         else:
             acceleration = np.zeros((speed.size))
         spike_data = store_acceleration_for_rewaded(spike_data, cluster, np.asarray(rates), np.asarray(position), np.asarray(speed), np.asarray(acceleration), np.asarray(trials), np.asarray(types))
+        plot_instant_acceleration_by_segment(recording_folder, spike_data, cluster, cluster_index, rates, position, speed, acceleration)
     return spike_data
 
 
@@ -163,13 +184,22 @@ def plot_instant_acceleration_by_segment(recording_folder, spike_data, cluster, 
         os.makedirs(save_path)
     rates_o, speed_o, position_o, acceleration_o, rates_h, speed_h, position_h, acceleration_h = remove_low_speeds_and_segment(rates, speed, position, acceleration )
 
+    rates =  pd.Series(rates_o)
+    acceleration =  pd.Series(acceleration_o)
+    speed =  pd.Series(speed_o)
+    position =  pd.Series(position_o)
+    rates_o = rates[speed.between(speed.quantile(.03), speed.quantile(.97))] # without outliers
+    acceleration_o = acceleration[speed.between(speed.quantile(.03), speed.quantile(.97))] # without outliers
+    position_o = position[speed.between(speed.quantile(.03), speed.quantile(.97))] # without outliers
+    speed_o = speed[speed.between(speed.quantile(.03), speed.quantile(.97))] # without outliers
+
     avg_spikes_on_track = plt.figure(figsize=(4,3))
     ax = avg_spikes_on_track.add_subplot(1, 1, 1)  # specify (nrows, ncols, axnum)
     area = np.pi*1
     plt.scatter(acceleration_o, rates_o, s=area, c=position_o)
     cbar=plt.colorbar()
     cbar.ax.tick_params(labelsize=16)
-    plt.ylabel('Spike rate (hz)', fontsize=16, labelpad = 10)
+    plt.ylabel('Firing rate (hz)', fontsize=16, labelpad = 10)
     plt.xlabel('Acceleration $(cm/s^2)$', fontsize=16, labelpad = 10) # "meters $10^1$"
     #ax.set_xlim(-20, 25)
     #ax.set_ylim(0)
@@ -203,7 +233,7 @@ def plot_instant_acceleration_by_segment(recording_folder, spike_data, cluster, 
     plt.scatter(acceleration_h, rates_h, s=area, c=position_h)
     cbar=plt.colorbar()
     cbar.ax.tick_params(labelsize=16)
-    plt.ylabel('Spike rate (hz)', fontsize=16, labelpad = 10)
+    plt.ylabel('Firing rate (hz)', fontsize=16, labelpad = 10)
     plt.xlabel('Acceleration $(cm/s)^2$', fontsize=16, labelpad = 10) # "meters $10^1$"
     #ax.set_xlim(-100, 150)
     ax.set_ylim(0)
@@ -230,6 +260,75 @@ def plot_instant_acceleration_by_segment(recording_folder, spike_data, cluster, 
     plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
     plt.savefig(save_path + '/' + spike_data.session_id[cluster] + '_rate_map_Cluster_' + str(cluster_index +1) + '_acceleration' + '_coded_homebound.png', dpi=200)
     plt.close()
+
+    avg_spikes_on_track = plt.figure(figsize=(4,3))
+    ax = avg_spikes_on_track.add_subplot(1, 1, 1)  # specify (nrows, ncols, axnum)
+    area = np.pi*1
+    plt.scatter(speed_o, rates_o, s=area, c=position_o)
+    cbar=plt.colorbar()
+    cbar.ax.tick_params(labelsize=16)
+    plt.ylabel('Firing rate (hz)', fontsize=16, labelpad = 10)
+    plt.xlabel('Speed $(cm/s)$', fontsize=16, labelpad = 10) # "meters $10^1$"
+    #ax.set_xlim(-20, 25)
+    #ax.set_ylim(0)
+    ax.tick_params(
+        axis='both',  # changes apply to the x-axis
+        which='both',  # both major and minor ticks are affected
+        bottom=True,  # ticks along the bottom edge are off
+        top=False,  # ticks along the top edge are off
+        right=False,
+        left=True,
+        labelleft=True,
+        labelbottom=True,
+        labelsize=16,
+        length=5,
+        width=1.5)  # labels along the bottom edge are off
+    ax.locator_params(axis = 'x', nbins=3)
+    plt.locator_params(axis = 'y', nbins  = 4)
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_visible(True)
+    ax.spines['bottom'].set_visible(True)
+    ax.spines['left'].set_linewidth(1.5)
+    ax.spines['bottom'].set_linewidth(1.5)
+    plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
+    plt.savefig(save_path + '/' + spike_data.session_id[cluster] + '_rate_map_Cluster_' + str(cluster_index +1) + '_speed' + '_coded_outbound.png', dpi=200)
+    plt.close()
+
+    avg_spikes_on_track = plt.figure(figsize=(4,3))
+    ax = avg_spikes_on_track.add_subplot(1, 1, 1)  # specify (nrows, ncols, axnum)
+    area = np.pi*1
+    plt.scatter(position_o, rates_o, s=area, c=position_o)
+    cbar=plt.colorbar()
+    cbar.ax.tick_params(labelsize=16)
+    plt.ylabel('Firing rate (hz)', fontsize=16, labelpad = 10)
+    plt.xlabel('Location $(cm)$', fontsize=16, labelpad = 10) # "meters $10^1$"
+    #ax.set_xlim(-20, 25)
+    #ax.set_ylim(0)
+    ax.tick_params(
+        axis='both',  # changes apply to the x-axis
+        which='both',  # both major and minor ticks are affected
+        bottom=True,  # ticks along the bottom edge are off
+        top=False,  # ticks along the top edge are off
+        right=False,
+        left=True,
+        labelleft=True,
+        labelbottom=True,
+        labelsize=16,
+        length=5,
+        width=1.5)  # labels along the bottom edge are off
+    ax.locator_params(axis = 'x', nbins=3)
+    plt.locator_params(axis = 'y', nbins  = 4)
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_visible(True)
+    ax.spines['bottom'].set_visible(True)
+    ax.spines['left'].set_linewidth(1.5)
+    ax.spines['bottom'].set_linewidth(1.5)
+    plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
+    plt.savefig(save_path + '/' + spike_data.session_id[cluster] + '_rate_map_Cluster_' + str(cluster_index +1) + '_position' + '_coded_outbound.png', dpi=200)
+    plt.close()
+
     return
 
 
