bccea84733111901c7b91f8c2b500f4d6045c199
sarahtennant <stennan2@exseed.ed.ac.uk>
2022-01-25 16:03:51 +0000

    simplify code for calculating progression

--- Python_PostSorting/BehaviourAnalysis.py
+++ Python_PostSorting/BehaviourAnalysis.py
@@ -18,22 +18,24 @@ def calculate_graduation_day(spike_data):
 
 def calculate_progression(spike_data):
     print('calculating graduation day')
-    select_df = spike_data[["Mouse", "Day_numeric", "probe_trials", "Reward_Rate"]]
+    select_df = spike_data[["Mouse", "Day_numeric", "cohort","probe_trials", "Reward_Rate", "FirstStop"]]
+    df = select_df.sort_values(['Mouse','Day_numeric'])
 
-    unique_mice = np.unique(np.array(select_df["Mouse"]))
+   # unique_mice = np.unique(np.array(select_df["Mouse"]))
 
-    for m, mcount in enumerate(unique_mice):
-        mouse_df = select_df.loc[select_df["Mouse"] == mcount, ["Mouse","Day_numeric", "probe_trials", "Reward_Rate"]]
-        df = mouse_df.sort_values('Day_numeric')
-        write_to_csv(np.array(df), mcount)
+    #for m, mcount in enumerate(unique_mice):
+        #mouse_df = select_df.loc[select_df["Mouse"] == mcount, ["Mouse","Day_numeric", "probe_trials", "Reward_Rate"]]
+        #df = mouse_df.sort_values('Day_numeric')
+
+    write_to_csv(np.array(df))
 
     return spike_data
 
 
 
 ## Save to .csv file
-def write_to_csv(csvData,m):
-    with open('/Users/sarahtennant/Work/Analysis/Ramp_analysis/data/graduation-' + str(m) + '.csv', 'w') as csvFile:
+def write_to_csv(csvData):
+    with open('/Users/sarahtennant/Work/Analysis/Ramp_analysis/data/graduation-' + '7' + '.csv', 'w') as csvFile:
         writer = csv.writer(csvFile)
         writer.writerows(csvData)
     csvFile.close()
