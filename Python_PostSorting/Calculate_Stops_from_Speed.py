385cd2b7a5abe5d05c676dd866ac1c1c29ccac03
sarahtennant <stennan2@exseed.ed.ac.uk>
2022-01-25 16:04:17 +0000

    calculate the time of stops and rewards

--- Python_PostSorting/Calculate_Stops_from_Speed.py
+++ Python_PostSorting/Calculate_Stops_from_Speed.py
@@ -28,7 +28,7 @@ def calculate_stops_from_speed(spike_data):
         stop_types = []
 
         for rowcount, row in enumerate(data):
-            if data[rowcount,0] < 10.7 :
+            if data[rowcount,0] < 11.7 :
                 stop_positions = np.append(stop_positions, data[rowcount,1])
                 stop_trials = np.append(stop_trials, data[rowcount,3])
                 stop_types = np.append(stop_types, data[rowcount,2])
@@ -43,37 +43,99 @@ def calculate_stops_from_speed(spike_data):
 
 
 
-def calculate_stops_from_200ms_speed(spike_data):
+def calculate_stop_time_from_speed(spike_data):
     print("calculating stops from speed...")
     spike_data["stops_cm_position"] = ""
     spike_data["stops_cm_trial"] = ""
     spike_data["stops_cm_trial_type"] = ""
+    spike_data["stops_cm_time"] = ""
 
     for cluster in range(len(spike_data)):
         speed=np.array(spike_data.iloc[cluster].spike_rate_in_time[1].real)
         position=np.array(spike_data.iloc[cluster].spike_rate_in_time[2].real)
         types=np.array(spike_data.iloc[cluster].spike_rate_in_time[4].real, dtype= np.int32)
         trials=np.array(spike_data.iloc[cluster].spike_rate_in_time[3].real, dtype= np.int32)
-
-        data = np.vstack((speed,position,types, trials))
+        time = np.arange(100,len(trials)*100,100)
+        data = np.vstack((speed,position,types, trials, time))
         data=data.transpose()
 
         stop_positions = []
         stop_trials = []
         stop_types = []
+        stop_times = []
 
-        for rowcount, row in enumerate(range(len(data)-1)):
-            speed_200ms = data[rowcount,0] + data[rowcount+1,0]
-            if speed_200ms < 11.7:
+        for rowcount, row in enumerate(data):
+            if data[rowcount,0] < 4.7 :
                 stop_positions = np.append(stop_positions, data[rowcount,1])
                 stop_trials = np.append(stop_trials, data[rowcount,3])
                 stop_types = np.append(stop_types, data[rowcount,2])
+                stop_times = np.append(stop_times, data[rowcount,4])
 
-        stop_positions, stop_trials, stop_types = remove_extra_stops(2, stop_positions, stop_trials, stop_types)
+        stop_positions, stop_trials, stop_types = remove_extra_stops_with_time(2, stop_positions, stop_trials, stop_types, stop_times)
 
         spike_data.at[cluster, 'stops_cm_position'] = list(stop_positions)# add data to dataframe
         spike_data.at[cluster, 'stops_cm_trial'] = list(stop_trials)# add data to dataframe
         spike_data.at[cluster, 'stops_cm_trial_type'] = list(stop_types)# add data to dataframe
+        spike_data.at[cluster, 'stops_cm_time'] = list(stop_times)# add data to dataframe
+
+    return spike_data
+
+
+def remake_times_on_trials(time, trials):
+    data = np.vstack((time, trials))
+    data=data.transpose()
+
+    # bin data over position bins
+    trial_numbers = np.unique(trials)
+    new_time_array = np.zeros((0)); new_time_array[:] = np.nan
+    for trialcount, trial in enumerate(trial_numbers):
+        trial_data = data[data[:,1] == trial,:]
+        trial_start_time = trial_data[0,0]
+        for rowcount, row in enumerate(trial_data+1):
+            new_time = row[0] - trial_start_time
+            new_time_array = np.append(new_time_array, new_time)
+
+    return new_time_array
+
+
+def calculate_stops_from_200ms_speed(spike_data):
+    print("calculating stops from speed...")
+    spike_data["stops_cm_position"] = ""
+    spike_data["stops_cm_trial"] = ""
+    spike_data["stops_cm_trial_type"] = ""
+    spike_data["stops_cm_time"] = ""
+
+    for cluster in range(len(spike_data)):
+        speed=np.array(spike_data.iloc[cluster].spike_rate_in_time[1].real)
+        position=np.array(spike_data.iloc[cluster].spike_rate_in_time[2].real)
+        types=np.array(spike_data.iloc[cluster].spike_rate_in_time[4].real, dtype= np.int32)
+        trials=np.array(spike_data.iloc[cluster].spike_rate_in_time[3].real, dtype= np.int32)
+        time = np.arange(100,(len(trials)*100)+1,100)
+
+        time = remake_times_on_trials(time, trials)
+
+        data = np.vstack((speed,position,types, trials, time))
+        data=data.transpose()
+
+        stop_positions = []
+        stop_trials = []
+        stop_types = []
+        stop_times = []
+
+        for rowcount, row in enumerate(range(len(data)-1)):
+            speed_200ms = data[rowcount,0] + data[rowcount+1,0]
+            if speed_200ms <= 4.7:
+                stop_positions = np.append(stop_positions, data[rowcount,1])
+                stop_trials = np.append(stop_trials, data[rowcount,3])
+                stop_types = np.append(stop_types, data[rowcount,2])
+                stop_times = np.append(stop_times, data[rowcount,4])
+
+        stop_positions, stop_trials, stop_types, stop_times = remove_extra_stops_with_time(2, stop_positions, stop_trials, stop_types, stop_times)
+
+        spike_data.at[cluster, 'stops_cm_position'] = list(stop_positions)# add data to dataframe
+        spike_data.at[cluster, 'stops_cm_trial'] = list(stop_trials)# add data to dataframe
+        spike_data.at[cluster, 'stops_cm_trial_type'] = list(stop_types)# add data to dataframe
+        spike_data.at[cluster, 'stops_cm_time'] = list(stop_times)# add data to dataframe
 
     return spike_data
 
@@ -95,36 +157,64 @@ def remove_extra_stops(min_distance, stops, trials, types):
     return new_stops, new_trials, new_types
 
 
+def remove_extra_stops_with_time(min_distance, stops, trials, types, time):
+    new_stops = []
+    new_trials = []
+    new_types = []
+    new_times = []
+    for rowcount, row in enumerate(range(len(stops)-1)):
+        current_stop = stops[rowcount]
+        current_trial = trials[rowcount]
+        current_type = types[rowcount]
+        current_time = time[rowcount]
+        next_stop = stops[rowcount + 1]
+        if (next_stop - current_stop) > min_distance:
+            new_stops.append(current_stop)
+            new_trials.append(current_trial)
+            new_types.append(current_type)
+            new_times.append(current_time)
+
+    return new_stops, new_trials, new_types, new_times
+
+
+
 
 
 def calculate_rewards_from_stops(spike_data):
     print("calculating rewards from stops...")
-    spike_data["reward_position"] = ""
-    spike_data["reward_trial"] = ""
+    spike_data["rewarded_locations"] = ""
+    spike_data["rewarded_trials"] = ""
+    spike_data["rewarded_times"] = ""
 
     for cluster in range(len(spike_data)):
         stops = np.array(spike_data.at[cluster, 'stops_cm_position'], dtype=np.int16)
         trials = np.array(spike_data.at[cluster, 'stops_cm_trial'], dtype=np.int16)
+        time = np.array(spike_data.at[cluster, 'stops_cm_time'], dtype=np.int16)
 
-        data = np.vstack((stops, trials))
+        data = np.vstack((stops, trials, time))
         data=data.transpose()
 
         reward_positions = []
         reward_trials = []
+        reward_times = []
 
-        # bin data over position bins
-        trial_numbers = np.arange(min(trials),max(trials), 1)
-        for tcount, trial in enumerate(trial_numbers):
-            trial_data = data[data[:,1] == trial,:]
-            if trial_data.shape[0] > 0:
-                for rowcount, row in enumerate(data):
-                    if data[rowcount,0] > 88 and data[rowcount,0] < 110:
-                        reward_positions = np.append(reward_positions, data[rowcount,0])
-                        reward_trials = np.append(reward_trials, data[rowcount,1])
-                        continue
-                continue
-        spike_data.at[cluster, 'reward_position'] = list(reward_positions)# add data to dataframe
-        spike_data.at[cluster, 'reward_trial'] = list(reward_trials)# add data to dataframe
+        if len(np.unique(trials)) > 1:
+            # bin data over position bins
+            trial_numbers = np.arange(min(trials),max(trials), 1)
+            for tcount, trial in enumerate(trial_numbers):
+                trial_data = data[data[:,1] == trial,:]
+                if trial_data.shape[0] > 0:
+                    for rowcount, row in enumerate(trial_data):
+                        if trial_data[rowcount,0] >= 88 and trial_data[rowcount,0] <= 110:
+                            reward_positions = np.append(reward_positions, trial_data[rowcount,0])
+                            reward_trials = np.append(reward_trials, trial_data[rowcount,1])
+                            reward_times = np.append(reward_times, trial_data[rowcount,2])
+                            break
+                    break
+
+        spike_data.at[cluster, 'rewarded_locations'] = list(reward_positions)# add data to dataframe
+        spike_data.at[cluster, 'rewarded_trials'] = list(reward_trials)# add data to dataframe
+        spike_data.at[cluster, 'rewarded_times'] = list(reward_times)# add data to dataframe
 
     return spike_data
 
@@ -309,65 +399,5 @@ def calculate_average_spikes(spike_data):
     return spike_data
 
 
-
-
-
-
-def plot_firing_rate_probe(spike_data, prm):
-    save_path = prm.get_local_recording_folder_path() + '/Figures/Firing_Rate_Maps_Probe2'
-    if os.path.exists(save_path) is False:
-        os.makedirs(save_path)
-
-    for cluster in range(len(spike_data)):
-        cluster_index = spike_data.cluster_id.values[cluster] - 1
-        position_array=np.arange(0,200,1)
-        rates=np.array(spike_data.loc[cluster, 'average_spikes'])*20
-        #sd_rates=np.array(spike_data.loc[cluster, 'Rates_sd_rewarded'])
-        rates_p=np.array(spike_data.loc[cluster, 'average_spikes_probe'])*20
-        #sd_rates_p=np.array(spike_data.loc[cluster, 'Rates_sd_rewarded-p'])
-
-        speed_histogram = plt.figure(figsize=(3.7,3))
-        ax = speed_histogram.add_subplot(1, 1, 1)  # specify (nrows, ncols, axnum)
-        ax.plot(position_array,rates, '-', color='Black')
-        ax.fill_between(position_array, rates-3.3,rates+3.3, facecolor = 'Black', alpha = 0.2)
-        ax.plot(position_array,rates_p, '-', color='Blue')
-        ax.fill_between(position_array, rates_p-3.2,rates_p+3.2, facecolor = 'Blue', alpha = 0.2)
-
-        plt.ylabel('Firing rate (hz)', fontsize=18, labelpad = 0)
-        plt.xlabel('Location (cm)', fontsize=18, labelpad = 10)
-        plt.xlim(0,200)
-        ax.yaxis.set_ticks_position('left')
-        ax.xaxis.set_ticks_position('bottom')
-        Python_PostSorting.plot_utility.style_track_plot(ax, 200)
-        Python_PostSorting.plot_utility.style_vr_plot(ax)
-        ax.set_ylim(0)
-        plt.locator_params(axis = 'y', nbins  = 4)
-        plt.locator_params(axis = 'x', nbins  = 3)
-        ax.set_xticklabels(['-30', '70', '170'])
-        plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
-        plt.savefig(save_path + '/time_binned_Rates_histogram_' + spike_data.session_id[cluster] + '_' + str(cluster_index +1) + 'rewarded_p.png', dpi=200)
-        plt.close()
-
-        speed_histogram = plt.figure(figsize=(3.7,3))
-        ax = speed_histogram.add_subplot(1, 1, 1)  # specify (nrows, ncols, axnum)
-        ax.fill_between(position_array, rates-3.3,rates+3.3, facecolor = 'Black', alpha = 0.2)
-        ax.plot(position_array,rates, '-', color='Black')
-
-        plt.ylabel('Firing rate (hz)', fontsize=18, labelpad = 0)
-        plt.xlabel('Location (cm)', fontsize=18, labelpad = 10)
-        plt.xlim(0,200)
-        ax.yaxis.set_ticks_position('left')
-        ax.xaxis.set_ticks_position('bottom')
-        Python_PostSorting.plot_utility.style_track_plot(ax, 200)
-        Python_PostSorting.plot_utility.style_vr_plot(ax)
-        ax.set_ylim(0)
-        plt.locator_params(axis = 'y', nbins  = 4)
-        plt.locator_params(axis = 'x', nbins  = 3)
-        ax.set_xticklabels(['-30', '70', '170'])
-        plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
-        plt.savefig(save_path + '/time_binned_Rates_histogram_' + spike_data.session_id[cluster] + '_' + str(cluster_index +1) + 'rewarded.png', dpi=200)
-        plt.close()
-
-    return spike_data
 
 
